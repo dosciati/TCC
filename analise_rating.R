ec0a84abda9985cf9acb338d814e23d380748e6a
André Forlin Dosciati <dosciati@hotmail.com>
2023-01-12 09:46:27 -0300

    apenas um teste

---
+++ analise_rating.R
@@ -0,0 +1,31 @@
+#PROJETO TCC
+#Análise e classificação de empresas usando balanço patrimonial.
+#André Forlin Dosciati
+#10/12/2022
+#MBA USP / ESALQ
+
+library(readr)
+corporate_rating_traduzido <- read_csv("base de dados/archive/corporate_rating_traduzido.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
