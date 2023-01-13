85cc6a52a7af5165ba1d587a03ee5f46a55cdec3
André Forlin Dosciati <dosciati@hotmail.com>
2023-01-13 12:20:43 -0300

    Carregamento da base com a conversão de tipo das colunas numéricas. Iniciei processo de treinamento separando a base em amostra e treino.

--- analise_rating.R
+++ analise_rating.R
@@ -4,28 +4,35 @@
 #10/12/2022
 #MBA USP / ESALQ
 
-library(readr)
-corporate_rating_traduzido <- read_csv("base de dados/archive/corporate_rating_traduzido.csv")
 
-#teste de commit 120123
+# Load libraries for data manipulation and visualization
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+# Load the data into a dataframe
+df <- read_excel("base de dados/archive/Pasta11.xlsx", 
+                      col_types = c("text", "text", "text", 
+                                     "text", "text", "text", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric"))
+str(df)
+df$classificação
+df$setor
+df$`ebit por receita`
+prop.table(table(df$setor))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#teste de algoritmo - preparando as amostras de treino.
+set.seed(1234)
+amostra <- sample(1:nrow(df), nrow(df) * 0.7, replace = FALSE)
+dados_treino <- df[amostra, ]
+dados_treino$classificação <- as.factor(dados_treino$classificação)
+dados_teste <- df[-amostra, ]
+dados_teste$classificação <- as.factor(dados_teste$classificação)
 
